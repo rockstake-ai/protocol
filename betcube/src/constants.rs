267acce88016146e1af34f40193893679243250c
Andrei-Constantin Chirita <56071527+andrewkhirita@users.noreply.github.com>
2024-09-16 22:19:52 +0300

    Delete betcube directory

--- betcube/src/constants.rs
+++
@@ -1,8 +0,0 @@
-pub const IPFS_GATEWAY: &[u8] = "https://ipfs.io/ipfs/".as_bytes();
-
-pub const NFT_ISSUE_COST: u64 = 50_000_000_000_000_000; // 0.05 EGLD
-pub const ROYALTIES_MAX: u32 = 10;
-pub const NFT_AMOUNT: u32 = 1;
-
-pub const TOKEN_NAME: &[u8] = b"BetCube";
-pub const TOKEN_TICKER: &[u8] = b"BET";
