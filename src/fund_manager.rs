0bac9f7a146236c6cd6aaa83434456134e5e3ffb
andrewkhirita <andrei.chirita2013@gmail.com>
2024-09-28 13:50:37 +0300

    design lock_funds & distribute_winnings

--- src/fund_manager.rs
+++ src/fund_manager.rs
@@ -1,4 +1,4 @@
-use crate::storage;
+use crate::storage::{self, Status};
 
 multiversx_sc::imports!();
 multiversx_sc::derive_imports!();
@@ -6,13 +6,32 @@ multiversx_sc::derive_imports!();
 #[multiversx_sc::module]
 pub trait FundManagerModule: storage::StorageModule{
     #[payable("*")]
-    #[endpoint]
+    #[endpoint(lockFunds)]
     fn lock_funds(&self, user: ManagedAddress, amount: BigUint) {
+        let caller = self.blockchain().get_caller();
+        let payment_amount = self.call_value().egld_or_single_fungible_esdt();  // Obținem suma plătită în EGLD
+        
+        // require!(payment_amount == amount, "Suma trimisă nu corespunde cu cea specificată");
+    
+        self.locked_funds(&user).update(|current_locked| *current_locked += payment_amount);
     }
-
-    // Funcție pentru distribuirea câștigurilor după finalizarea evenimentului
+    
     #[only_owner]
-    #[endpoint]
+    #[endpoint(distributeWinnings)]
     fn distribute_winnings(&self, market_id: BigUint, winner_address: ManagedAddress) {
-    }
+        let market = self.markets(&market_id).get();  // Obținem piața respectivă
+        let mut total_payout = BigUint::zero();
+
+        for bet in market.bets.iter() {
+            if bet.status == Status::Win && bet.user == winner_address {
+            let payout = bet.value.clone() * bet.odd.clone();  // câștig = suma pariată * cota
+            total_payout += payout;
+
+            // Actualizăm fondurile blocate ale câștigătorului
+            self.locked_funds(&winner_address).update(|current_locked| *current_locked -= bet.value.clone());
+            }
+        };
+        self.send().direct(&winner_address, &total_payout);
+}
+
 }
