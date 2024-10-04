53959b7d805944b2c43126a821b25c85e7ce45f2
andrewkhirita <andrei.chirita2013@gmail.com>
2024-10-04 17:02:28 +0300

    update fund_manager

--- src/fund_manager.rs
+++ src/fund_manager.rs
@@ -17,24 +17,24 @@ pub trait FundManagerModule:
         let mut bet = self.require_valid_bet_nft(bet_id);
         require!(bet.status == BetStatus::Win, "Bet is not winning state");
  
-        // let amount_to_distribute = match bet.status {
-        //     BetStatus::Win => match bet.bet_type {
-        //         BetType::Back => bet.win_amount.clone(),
-        //         BetType::Lay => &bet.stake_amount - &bet.win_amount,
-        //     },
-        //     BetStatus::Unmatched => bet.stake_amount.clone(),
-        //     _ => sc_panic!("Bet is not in a state eligible for distribution"),
-        // };
+        let amount_to_distribute = match bet.status {
+            BetStatus::Win => match bet.bet_type {
+                BetType::Back => bet.potential_profit.clone(),
+                BetType::Lay => &bet.stake_amount - &bet.potential_profit,
+            },
+            BetStatus::Unmatched => bet.stake_amount.clone(),
+            _ => sc_panic!("Bet is not in a state eligible for distribution"),
+        };
 
-        // require!(amount_to_distribute > BigUint::zero(), "No winnings to distribute");
+        require!(amount_to_distribute > BigUint::zero(), "No winnings to distribute");
 
-        // let payment = EgldOrEsdtTokenPayment::new(bet.payment_token, bet.payment_nonce, amount_to_distribute);
+        let payment = EgldOrEsdtTokenPayment::new(bet.payment_token, bet.payment_nonce, amount_to_distribute);
 
-        // self.send().direct(
-        //     &bet.bettor,
-        //     &payment.token_identifier,
-        //     payment.token_nonce,
-        //     &payment.amount,
-        // );
+        self.send().direct(
+            &bet.bettor,
+            &payment.token_identifier,
+            payment.token_nonce,
+            &payment.amount,
+        );
     }
 }
