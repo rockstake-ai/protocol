2e4aa5c435b079afdcf4771261e1dfd1df98d952
andrewkhirita <andrei.chirita2013@gmail.com>
2024-10-29 16:42:32 +0200

    fix bet_manager

--- src/bet_manager.rs
+++ src/bet_manager.rs
@@ -8,133 +8,134 @@ pub trait BetManagerModule: crate::storage::StorageModule
     + crate::nft_manager::NftManagerModule
     + crate::tracker::TrackerModule {
 
-    #[payable("*")]
-    #[endpoint(placeBet)]
-    fn place_bet(
-        &self,
-        market_id: u64,
-        selection_id: u64,
-        odds: BigUint,
-        bet_type: BetType
-    ) -> SCResult<(u64, BigUint, BigUint)> {
-        let mut market = self.markets(&market_id).get();
-        let created_at = self.blockchain().get_block_timestamp();
-    
-        require!(!self.markets(&market_id).is_empty(), ERR_INVALID_MARKET);
-        require!(market.market_status == MarketStatus::Open, ERR_MARKET_NOT_OPEN);
-        require!(created_at < market.close_timestamp, ERR_MARKET_CLOSED);
-        require!(
-            odds >= BigUint::from(101u32) && odds <= BigUint::from(100000u32),
-            ERR_BET_ODDS
-        );
-    
-        let caller = self.blockchain().get_caller();
-        let (token_identifier, token_nonce, total_amount) = self
-            .call_value()
-            .egld_or_single_esdt()
-            .into_tuple();
-        let bet_id = self.get_last_bet_id() + 1;
-    
-        let (stake, liability) = match bet_type {
-            BetType::Back => {
-                (total_amount.clone(), BigUint::zero())
-            },
-            BetType::Lay => {
-                let stake = self.calculate_stake_from_total(&total_amount, &odds);
-                let calculated_liability = self.calculate_potential_liability(&bet_type, &stake, &odds);
-                
-                require!(
-                    stake.clone() + calculated_liability.clone() <= total_amount,
-                    "Invalid total amount for Lay bet"
-                );
-                
-                (stake, calculated_liability)
+        #[payable("*")]
+        #[endpoint(placeBet)]
+        fn place_bet(
+            &self,
+            market_id: u64,
+            selection_id: u64,
+            odds: BigUint,
+            bet_type: BetType
+        ) -> SCResult<(u64, BigUint, BigUint)> {
+            let mut market = self.markets(&market_id).get();
+            let created_at = self.blockchain().get_block_timestamp();
+        
+            // Validări
+            require!(!self.markets(&market_id).is_empty(), ERR_INVALID_MARKET);
+            require!(market.market_status == MarketStatus::Open, ERR_MARKET_NOT_OPEN);
+            require!(created_at < market.close_timestamp, ERR_MARKET_CLOSED);
+            require!(
+                odds >= BigUint::from(101u32) && odds <= BigUint::from(100000u32),
+                ERR_BET_ODDS
+            );
+        
+            let caller = self.blockchain().get_caller();
+            let (token_identifier, token_nonce, total_amount) = self
+                .call_value()
+                .egld_or_single_esdt()
+                .into_tuple();
+            let bet_id = self.get_last_bet_id() + 1;
+        
+            let (stake, liability) = match bet_type {
+                BetType::Back => {
+                    (total_amount.clone(), BigUint::zero())
+                },
+                BetType::Lay => {
+                    let stake = self.calculate_stake_from_total(&total_amount, &odds);
+                    let calculated_liability = self.calculate_potential_liability(&bet_type, &stake, &odds);
+                    
+                    require!(
+                        stake.clone() + calculated_liability.clone() <= total_amount,
+                        "Invalid total amount for Lay bet"
+                    );
+                    
+                    (stake, calculated_liability)
+                }
+            };
+        
+            let selection_index = market
+                .selections
+                .iter()
+                .position(|s| &s.selection_id == &selection_id)
+                .expect(ERR_SELECTION);
+            let mut selection = market.selections.get(selection_index);
+            
+            // Inițializăm selection scheduler dacă nu există
+            if self.selection_scheduler(market_id, selection_id).is_empty() {
+                self.selection_scheduler(market_id, selection_id).set(&self.init_bet_scheduler());
             }
-        };
-    
-        let selection_index = market
-            .selections
-            .iter()
-            .position(|s| &s.selection_id == &selection_id)
-            .expect(ERR_SELECTION);
-        let mut selection = market.selections.get(selection_index);
-        self.bet_scheduler().set(selection.priority_queue.clone());
-    
-        let bet = Bet {
-            bettor: caller.clone(),
-            event: market_id,
-            selection: selection.clone(),
-            stake_amount: stake.clone(),
-            liability: match bet_type {
-                BetType::Back => BigUint::zero(),
-                BetType::Lay => liability.clone(),
-            },
-            matched_amount: BigUint::zero(),
-            unmatched_amount: match bet_type {
-                BetType::Back => stake.clone(),
-                BetType::Lay => liability.clone(),
-            },
-            potential_profit: self.calculate_potential_profit(&bet_type, &stake, &odds),
-            odd: odds.clone(),
-            bet_type: bet_type.clone(),
-            status: BetStatus::Unmatched,
-            payment_token: token_identifier.clone(),
-            payment_nonce: token_nonce,
-            nft_nonce: bet_id,
-            created_at: created_at
-        };
-    
-        // Folosim direct metodele din BetSchedulerModule
-        let (matched_amount, unmatched_amount, updated_bet) = self.match_bet(bet);
         
-        // Actualizăm selection cu noua stare
-        let updated_scheduler = self.bet_scheduler().get();
-
-        self.counter_debug_event(
-            &updated_scheduler.matched_count,
-            &updated_scheduler.unmatched_count,
-            &updated_scheduler.partially_matched_count
-        );
-
-        selection.priority_queue = updated_scheduler;
+            let bet = Bet {
+                bettor: caller.clone(),
+                event: market_id,
+                selection: selection.clone(),
+                stake_amount: stake.clone(),
+                liability: match bet_type {
+                    BetType::Back => BigUint::zero(),
+                    BetType::Lay => liability.clone(),
+                },
+                matched_amount: BigUint::zero(),
+                unmatched_amount: match bet_type {
+                    BetType::Back => stake.clone(),
+                    BetType::Lay => liability.clone(),
+                },
+                potential_profit: self.calculate_potential_profit(&bet_type, &stake, &odds),
+                odd: odds.clone(),
+                bet_type: bet_type.clone(),
+                status: BetStatus::Unmatched,
+                payment_token: token_identifier.clone(),
+                payment_nonce: token_nonce,
+                nft_nonce: bet_id,
+                created_at: created_at
+            };
         
-        let bet_nft_nonce = self.mint_bet_nft(&updated_bet);
-        self.bet_by_id(bet_id).set(&updated_bet);
-    
-        let _ = market.selections.set(selection_index, &selection);
-        market.total_matched_amount += &matched_amount;
-        self.markets(&market_id).set(&market);
-    
-        let total_locked = match bet_type {
-            BetType::Back => unmatched_amount.clone(),
-            BetType::Lay => liability.clone(),
-        };
-        self.locked_funds(&caller).update(|current_locked| *current_locked += &total_locked);
-    
-        self.send().direct_esdt(
-            &caller,
-            self.bet_nft_token().get_token_id_ref(),
-            bet_nft_nonce,
-            &BigUint::from(1u64)
-        );
-    
-        self.place_bet_event(
-            &caller,
-            self.bet_nft_token().get_token_id_ref(),
-            &market_id,
-            &selection_id,
-            &stake,
-            &odds,
-            bet_type.clone(),
-            &token_identifier,
-            token_nonce,
-            &matched_amount,
-            &unmatched_amount,
-            &liability
-        );
-    
-        Ok((bet_id, odds, stake))
-    }
+            // Match bet va folosi selection_scheduler intern
+            let (matched_amount, unmatched_amount, updated_bet) = self.match_bet(bet);
+            
+            // Luăm starea actualizată din selection_scheduler
+            let updated_scheduler = self.selection_scheduler(market_id, selection_id).get();
+            
+            // Actualizăm selection cu noua stare
+            selection.priority_queue = updated_scheduler;
+            
+            let bet_nft_nonce = self.mint_bet_nft(&updated_bet);
+            self.bet_by_id(bet_id).set(&updated_bet);
+        
+            let _ = market.selections.set(selection_index, &selection);
+            market.total_matched_amount += &matched_amount;
+            self.markets(&market_id).set(&market);
+        
+            let total_locked = match bet_type {
+                BetType::Back => unmatched_amount.clone(),
+                BetType::Lay => liability.clone(),
+            };
+            self.locked_funds(&caller).update(|current_locked| *current_locked += &total_locked);
+        
+            // Events și returnare
+            self.send().direct_esdt(
+                &caller,
+                self.bet_nft_token().get_token_id_ref(),
+                bet_nft_nonce,
+                &BigUint::from(1u64)
+            );
+        
+            self.place_bet_event(
+                &caller,
+                self.bet_nft_token().get_token_id_ref(),
+                &market_id,
+                &selection_id,
+                &stake,
+                &odds,
+                bet_type.clone(),
+                &token_identifier,
+                token_nonce,
+                &matched_amount,
+                &unmatched_amount,
+                &liability
+            );
+        
+            Ok((bet_id, odds, stake))
+        }
 
 
     fn calculate_potential_profit(&self, bet_type: &BetType, stake: &BigUint, odds: &BigUint) -> BigUint {
